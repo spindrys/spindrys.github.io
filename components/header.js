f283329625328d72ba4972ba017216d5ca74a6af
spindrys <94639417+spindrys@users.noreply.github.com>
2022-01-18 16:12:18 -0500

    Add files via upload
    initial commit

---
+++ components/header.js
@@ -0,0 +1,43 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+
+<link rel="stylesheet" href="styles.css" />
+
+<div class="nav-wrapper">
+          <div class="left">
+            <div class="nav-link-wrapper">
+              <a href="index.html">Home</a>
+            </div>
+            <div class="nav-link-wrapper">
+              <a href="about.html">About</a>
+            </div>
+          </div>
+  
+          <div class="right">
+            <div class="brand">
+              <div>Sophie Pindrys</div>
+            </div>
+          </div>
+        </div> 
+
+`
+
+
+
+
+class Header extends HTMLElement {
+    constructor() {
+        super(); 
+    }
+
+    connectedCallback() {
+       const shadowRoot = this.attachShadow({mode: 'closed'})
+       shadowRoot.appendChild(headerTemplate.content);
+       
+        
+        
+    }
+}
+
+customElements.define('header-component', Header);
